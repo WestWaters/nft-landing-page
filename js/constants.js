acee20476ad8a20d25decd46aed40dee2464d6e6
WestWaters <Mario.papaleo2013@gmail.com>
2022-03-29 20:25:46 -0700

    Html Update

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const chain = 'Ethereum'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Zoinked Punks PFP NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
